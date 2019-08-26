2a12c6c3096714e3ff9bff92cebada273c2a381c
udayakumaran <imudaya3250@gmail.com>
2019-08-26 18:33:01 +0530

    second version

--- Number Detection and Extraction.py
+++ Number Detection and Extraction.py
@@ -19,66 +19,66 @@ from tqdm import tqdm
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 # dataset Path
-data = pd.read_json(r"E:\uday\humAIn\vehicle dataset\Indian_Number_plates.json", lines=True)
+dataset = pd.read_json(r"E:\uday\humAIn\vehicle dataset\Indian_Number_plates.json", lines=True)
 pd.set_option('display.max_colwidth', -1)
 # print(data)
 
 
 # Delete the empty column
-del data['extras']
+del dataset['extras']
 
 # Extract the points of the bounding boxes because thats what we want
-data['points'] = data.apply(lambda row: row['annotation'][0]['points'], axis=1)
+dataset['points'] = dataset.apply(lambda row: row['annotation'][0]['points'], axis=1)
 
 # And delete the rest of the annotation info
-del data['annotation']
-Images = []
-Plates = []
+del dataset['annotation']
+Original_IMG = []
+Number_Plates = []
 
 # Training the Dataset
 def downloadTraining(df):
-    for index, row in df.iterrows():
+    for i, row in df.iterrows():
         # Getting the image from the URL
         resp = urllib.request.urlopen(row[0])
         im = np.array(Image.open(resp))
         # append the image to the training input array
-        Images.append(im)
+        Original_IMG.append(im)
 
         # Points of number Plate
-        x_point_top = row[1][0]['x'] * im.shape[1]
-        y_point_top = row[1][0]['y'] * im.shape[0]
-        x_point_bot = row[1][1]['x'] * im.shape[1]
-        y_point_bot = row[1][1]['y'] * im.shape[0]
+        x_top_pt = row[1][0]['x'] * im.shape[1]
+        y_top_pt = row[1][0]['y'] * im.shape[0]
+        x_bot_pt = row[1][1]['x'] * im.shape[1]
+        y_bot_pt = row[1][1]['y'] * im.shape[0]
 
         # Cut the plate from the image and use it as output
         carImage = Image.fromarray(im)
-        plateImage = carImage.crop((x_point_top, y_point_top, x_point_bot, y_point_bot))
-        Plates.append(plateImage)
+        Detected_plate = carImage.crop((x_top_pt, y_top_pt, x_bot_pt, y_bot_pt))
+        Number_Plates.append(Detected_plate)
         # grayscaled = cv2.cvtColor(plateImage, cv2.COLOR_BGR2GRAY)
         # retval, threshold = cv2.threshold(grayscaled, 10, 255, cv2.THRESH_BINARY)
         # cv2.imshow('original', img)
         # cv2.imshow('threshold', threshold)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
-        text = pytesseract.image_to_string(plateImage, lang='eng')
+        text = pytesseract.image_to_string(Detected_plate, lang='eng')
         print("Plate Number is  : " + text)
         cv2.waitKey(0)
 
 
-downloadTraining(data)
+downloadTraining(dataset)
 
 # Output Displaying part
 # Create figure and axes
-fig, ax = plt.subplots(2, 1, constrained_layout=True)
+figure, axis = plt.subplots(2, 1, constrained_layout=True)
 
 # Set title
-ax[0].set_title('')
-ax[1].set_title('')
+axis[0].set_title('')
+axis[1].set_title('')
 
 # Display the images
 n = int(input("Enter the car number between 0 - 237: "))
 
-ax[0].imshow(Images[n])
-ax[1].imshow(Plates[n])
+axis[0].imshow(Original_IMG[n])
+axis[1].imshow(Number_Plates[n])
 
 plt.show()
